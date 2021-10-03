911ff6a62d74285fee505bbddeaec595c56fd995
DQLok <wkhhkw01@gmail.com>
2021-10-03 17:16:32 +0700

    Working with Forms and Cloud job

--- lib/app/home/jobs/job_page.dart
+++ lib/app/home/jobs/job_page.dart
@@ -1,6 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:time_tracker_flutter_course/app/home/jobs/edit_job_page.dart';
+import 'package:time_tracker_flutter_course/app/home/jobs/job_list_tile.dart';
 import 'package:time_tracker_flutter_course/app/home/model/job.dart';
 import 'package:time_tracker_flutter_course/common_widgets/show_alert_dialog.dart';
 import 'package:time_tracker_flutter_course/common_widgets/show_exception_dialog.dart';
@@ -27,16 +29,6 @@ class JobsPage extends StatelessWidget {
     if (didRequestSignOut == true) _signOut(context);
   }
 
-  Future<void> _createJob(BuildContext context) async {
-    try {
-      final database = Provider.of<Database>(context, listen: false);
-      await database.createJob(Job(name: 'Blogging', ratePerHour: 10));
-    } on FirebaseException catch (e) {
-      showExceptionAlertDialog(context,
-          title: 'Operation failed', exception: e);
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -55,7 +47,7 @@ class JobsPage extends StatelessWidget {
       body: _buildContents(context),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: () => _createJob(context),
+        onPressed: () => EditJobPage.show(context,job: null),
       ),
     );
   }
@@ -67,13 +59,23 @@ class JobsPage extends StatelessWidget {
         builder: (context, snapshot) {
           if (snapshot.hasData) {
             final jobs = snapshot.data;
-            final children = jobs!.map((job) => Text(job!.name)).toList();
-            return ListView(children: children,);
+            final children = jobs!
+                .map((job) => JobListTile(
+                    job: job!, 
+                    onTap: () => EditJobPage.show(context, job:job)))
+                .toList();
+            return ListView(
+              children: children,
+            );
           }
-          if (snapshot.hasError){
-            return Center(child: Text('Some error occurred'),);
+          if (snapshot.hasError) {
+            return Center(
+              child: Text('Some error occurred'),
+            );
           }
-          return Center(child: CircularProgressIndicator(),);
+          return Center(
+            child: CircularProgressIndicator(),
+          );
         });
   }
 }
--- lib/services/firestore_service.dart
+++ lib/services/firestore_service.dart
@@ -3,17 +3,17 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class FirestoreService{
   FirestoreService._();
   static final instance=FirestoreService._();
-  
+
    Future<void> setData({String? path, Map<String, dynamic>? data}) async {
     final reference = FirebaseFirestore.instance.doc(path!);
     await reference.set(data!);
   }
 
-  Stream<List<T>> collectionStream<T>({required String path,required T Function(Map<String,dynamic> data) builder}){
+  Stream<List<T>> collectionStream<T>({required String path,required T Function(Map<String,dynamic> data,String documentId) builder}){
     final reference = FirebaseFirestore.instance.collection(path);
     final snapshots = reference.snapshots();
     return snapshots.map((snapshot) => snapshot.docs.map(
-      (snapshot) => builder(snapshot.data())    
+      (snapshot) => builder(snapshot.data(),snapshot.id)    
         ).toList());
   }
 }
